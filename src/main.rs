837a09959ab9618de2e7ce20b7513fda82d1c5a0
Ronan <67182906+drm-solister@users.noreply.github.com>
2022-05-08 20:44:58 -0400

    fixed

--- src/main.rs
+++ src/main.rs
@@ -11,46 +11,46 @@ fn main() {
     window.set_light(Light::StickToCamera);
     let framerate = 60.0;
     window.set_framerate_limit(Some(framerate as u64));
-    //window.set_background_color(0.255, 0.486, 0.620);
+    // window.set_background_color(1.0, 1.0, 1.0);
+    window.set_background_color(0.1, 0.1, 0.1);
 
     let red = Point3::new(1.0, 0.0, 0.0);
     let green = Point3::new(0.0, 1.0, 0.0);
     let blue = Point3::new(0.0, 0.0, 1.0);
 
     let origin = Point3::new(0.0, 0.0, 0.0);
-    let i10 = Point3::new(10.0, 0.0, 0.0);
-    let j10 = Point3::new(0.0, 10.0, 0.0);
-    let k10 = Point3::new(0.0, 0.0, 10.0);
-    
+    let i = Point3::new(100.0, 0.0, 0.0);
+    let j = Point3::new(0.0, 100.0, 0.0);
+    let k = Point3::new(0.0, 0.0, 100.0);
 
-    // let mut cube = window.add_cube(1.0, 1.0, 1.0);
-    // cube.set_color(1.0, 0.0, 0.0);
-    // let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
-
+    // starting conditions
     let mut x = 1.0;
-    let mut y = -1.0;
-    let mut z = 1.0;
+    let mut y = 0.0;
+    let mut z = 0.0;
 
+    // σ, ρ, and β (sigma, rho, beta)
     let a = 10.0;
     let b = 28.0;
-    let c = 2.6667;
+    let c = 2.66667;
 
-    let start = Point3::new(x,y,z);
-    const HISTORY_LEN: usize = 3; // will keep this many points
+    // number of points that will be recorded
+    const HISTORY_LEN: usize = 1000;
     let mut history: [Option<Point3<f32>>; HISTORY_LEN] = [None; HISTORY_LEN];
     let mut index = 0;
+    let start = Point3::new(x,y,z);
     history[index] = Some(start);
 
     while window.render() {
-        // cube.prepend_to_local_rotation(&rot);
 
-        // 10*unit vectors
-        window.draw_line(&origin, &i10, &red);
-        window.draw_line(&origin, &j10, &green);
-        window.draw_line(&origin, &k10, &blue);
+        // axes
+        window.draw_line(&origin, &i, &red);
+        window.draw_line(&origin, &j, &green);
+        window.draw_line(&origin, &k, &blue);
 
+        // index marks the front of the "queue"
         index = (index+1)%HISTORY_LEN;
 
+        // lorenz system
         let dx = a*(y-x)/framerate;
         let dy = (x*(b-z)-y)/framerate;
         let dz = (x*y-c*z)/framerate;
@@ -59,30 +59,23 @@ fn main() {
         y = y + dy;
         z = z + dz;
 
-        //println!("{:?}, {:?}, {:?}", x,y,z);
         let particle = Point3::new(x as f32, y as f32, z as f32);
         history[index] = Some(particle);
 
-        // index is front of queue
-        // index-1 is back of queue
-        // println!("front of queue at index {:?}", index);
-        // starts at the front of the queue
-        // iterates the length of the queue
+        // connect all the points in history
         for i in index+1..HISTORY_LEN+index {
+
             let first_index = i%HISTORY_LEN;
             let next_index = (i+1)%HISTORY_LEN;
-
-            // println!("first index: {:?}, second index: {:?}", first_index, next_index);
-
-            if next_index == 0 {
-                continue;
-            }
             
             if !history[next_index].is_none() && !history[first_index].is_none() {
-                window.draw_line(&history[first_index].unwrap(), &history[next_index].unwrap(), &red);
+
+                let progress = (i-index) as f32/(HISTORY_LEN) as f32;
+                let linecolor = Point3::new(1.0*progress, 0.0, 1.0*(1.0-progress));
+                
+                window.draw_line(&history[first_index].unwrap(), &history[next_index].unwrap(), &linecolor);
             }
             
         }
-        //window.draw_point(&particle, &red);
     }
 }
---
+++ src/readme.md
@@ -0,0 +1,4 @@
+lorenz model
+rendered using kiss3d crate
+
+![lorenz model image](./chaos.png)
